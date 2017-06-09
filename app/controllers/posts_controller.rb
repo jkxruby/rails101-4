554a70392ac09cfb1dbeb190404d5b41c99c82d3
jkxruby <jkxruby@gmail.com>
2017-06-09 21:14:57 +0800

    implement my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,51 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
   end
-  def create
-    @group = Group.find(params[:group_id])
-    @post = Post.new(post_params)
-    @post.group = @group
-    @post.user = current_user
 
-    if @post.save
-      redirect_to group_path(@group)
-    else
-      render :new
-    end
-  end
+def edit
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+  @post.group = @group
+end
 
-  private
-  def post_params
-    params.require(:post).permit(:content)
+def create
+  @group = Group.find(params[:group_id])
+  @post = Post.new(post_params)
+  @post.group = @group
+  @post.user = current_user
+  if @post.save
+    redirect_to group_path(@group)
+  else
+    render :new
   end
+end
+
+def update
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+  @post.group = @group
+  @post.user = current_user
+
+  if @post.update(post_params)
+    redirect_to account_posts_path, notice: "update success"
+  else
+    render :edit
+  end
+end
+
+def destroy
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+  @post.group = @group
+  @post.destroy
+  redirect_to account_posts_path, alert: "post deleted"
+end
+
+def post_params
+  params.require(:post).permit(:content)
+end
 
 end
